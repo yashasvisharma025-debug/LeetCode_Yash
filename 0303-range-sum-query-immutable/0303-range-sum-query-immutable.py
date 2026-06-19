a5bc0d4e33f85f2effb63dcb054523648e793328
Yashasvi Sharma <yashasvisharma025@gmail.com>
2026-06-19 23:59:24 +0530

    Time: 4 ms (59.54%), Space: 22.8 MB (37.13%) - LeetHub

---
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.py
@@ -0,0 +1,8 @@
+class NumArray:
+    def __init__(self, nums: list[int]):
+        self.prefix_sum=[0]
+        for num in nums:
+            self.prefix_sum.append(self.prefix_sum[-1]+num)
+    
+    def sumRange(self, left:int, right:int) ->int:
+        return self.prefix_sum[right+1]-self.prefix_sum[left]
